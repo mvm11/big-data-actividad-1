f001d3554e0fc64be383f974bc876f41ee930b22
Mateo <mvalenciaminota@gmail.com>
2024-06-23 00:51:19 -0500

    update main.py

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
+import requests
 from pymongo import MongoClient
-import pandas as pd
+from pymongo.errors import ConnectionFailure
+
+# URL de la API
+url = "https://datasets-server.huggingface.co/rows?dataset=Nicolybgs%2Fhealthcare_data&config=default&split=train&offset=0&length=100"
 
 # Conexión a MongoDB
-client = MongoClient('mongodb://user:root@localhost:27017/')
-db = client['health-care']
-collection = db['diagnostics']
+client = MongoClient("mongodb://user:root@localhost:27017/")
+db = client["health-care"]
+collection = db["diagnoses"]
 
-# Carga de datos (asumiendo que los datos están en un CSV)
-data = pd.read_csv('healthcare_data.csv')
-data_dict = data.to_dict("records")
-collection.insert_many(data_dict)
+# Obtener datos de la API
+response = requests.get(url)
+data = response.json()
 
-# Verificación de la carga de datos
-for doc in collection.find().limit(5):  # Muestra los primeros 5 documentos
-    print(doc)
+# Ingresar datos a MongoDB
+try:
+    result = collection.insert_many(data)
+    print(f'Data inserted with ids: {result.inserted_ids}')
+except Exception as e:
+    print(f'An error occurred: {e}')
+finally:
+    client.close()
